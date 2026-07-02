de8cad6eb1925a5dfbe33168cf169b537c2978eb
ledieungoc90-debug <ledieungoc90@gmail.com>
2026-07-01 20:36:50 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  images: {
+    localPatterns: [
+      {
+        pathname: "/api/directus-assets/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
